825d52d99cb6b0e6da805f948bad703b33c76da7
dayshawn terry <dayshawn21@gmail.com>
2021-06-30 16:59:24 -0500

    2 project

--- config/database.js
+++ config/database.js
@@ -7,7 +7,6 @@ module.exports = ({ env }) => ({
         uri: "mongodb+srv://dayshawn:dayday@cluster0.mkolz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
       },
       options: {
-        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
         ssl: true,
       },
     },
